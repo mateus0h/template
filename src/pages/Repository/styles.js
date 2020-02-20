98f9c60f4b3b43cac99bc7d2e757b4fab8cdc122
Mateus Aguiar <53477918+mateus0h@users.noreply.github.com>
2020-02-20 10:35:19 -0300

    Create styles.js

---
+++ src/pages/Repository/styles.js
@@ -0,0 +1,10 @@
+import styled from 'styled-components';
+import { Button, CardActions } from '@material-ui/core';
+
+export const ButtonSave = styled(Button)`
+  margin-right: 8px !important;
+`;
+
+export const CardButtonSave = styled(CardActions)`
+  justify-content: flex-end;
+`;
